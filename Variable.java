61c271e386c61937c380e8f36b438ff0f086bf12
siddiq <mdsiddiqkaladgi@gmail.com>
2024-10-30 17:50:28 +0530

    task_1 is added

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a=2;
+         int b=3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+
+    }
+}
